4a661841e53705b0571d5cba5c4392abbd53d410
Abhi <abbbbbbbbbbbhi@gmail.coim>
2019-07-24 11:08:05 -0700

    initial setup

---
+++ sthvClient/sthv/client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+
+namespace sthv
+{
+	public class client : BaseScript
+	{
+		public client()
+		{
+
+		}
+	}
+		
+}
