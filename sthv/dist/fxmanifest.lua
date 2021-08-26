9b1b2d32ca9235073e920871147d876794228a1b
abhiss <abbbbbbbbbbbhi@gmail.com>
2021-08-25 22:34:35 -0700

    fix dist dir structure on build

---
+++ sthv/dist/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'abhi'
+description 'A Survive the Hunt gamemode.'
+version '1.0.0'
+resource_type 'gametype' { name = 'Survive the Hunt' }
+
+loadscreen_manual_shutdown "yes"
+
+client_scripts{
+    'sthv.client.net.dll'
+}
+server_script 'sthv.server.net.dll'
+ui_page 'ui/index.html'
+loadscreen 'loadingscreen/loadingscreen.html'
+files {
+    'loadingscreen/*',
+    'ui/index.html',
+    'Newtonsoft.Json.dll',
+    'System.ValueTuple.dll',
+    'ui/style.css',
+    'ui/script.js',
+    'ui/assets/*',
+    'ui/modules/*.js'
+}
+
